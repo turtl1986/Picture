dbcbad5ea264d55795ac024fc4a2fe381c62a49e
Дарья Якимович <yakimovichdari@gmail.com>
2024-07-29 13:26:18 +0300

    add: add code

---
+++ share.js
@@ -0,0 +1,22 @@
+const pictures = document.querySelectorAll(".picture");
+
+const removeActiveClass = () => {
+  pictures.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+pictures.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const activeItem = document.querySelector(".picture.active");
+    if (activeItem && activeItem !== item) {
+      activeItem.classList.remove("active");
+    }
+
+    item.classList.toggle("active");
+  });
+});
+
+document.addEventListener("click", () => {
+  removeActiveClass();
+});
